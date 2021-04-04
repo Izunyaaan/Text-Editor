bfd8b369e707cbc956bbb0cd037678e440c53bef
Izunyaaan <kanebluerivers@gmail.com>
2021-04-04 22:28:00 +0800

    lets do this!

--- js/js.js
+++ js/js.js
@@ -1,6 +1,7 @@
 /*
     ============================================================
             The All Important Coffin Dance Meme + Listeners
+            100% Distinction material
     ============================================================
 */
 window.addEventListener('keydown', function(e) {
@@ -21,6 +22,7 @@ window.addEventListener('touchend', function(e) {
 });
 document.getElementById("textArea").addEventListener("input", function(e) {
     updateDate();
+    countWords(document.getElementById("textArea").innerText);
 });
 document.getElementById("fileName").addEventListener("input", function(e) {
     updateDate();
@@ -43,7 +45,16 @@ button.forEach(i => {
     ==============================================================
 */
 function styleIt(style) {
+    if (style == "reset") { return resetContent(); }
+    if (style == "rickRoll") {
+        let url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        window.open(url, '_blank').focus();
+    }
     document.execCommand(style);
+}
+
+function test() {
+    window.alert(document.getElementById("fileName").value);
 }
 
 //Partial personalized exec command
@@ -59,8 +70,7 @@ function wrapSelectedText(aStyle) {
             span.style.fontStyle = "italic";
             break;
         default:
-            span.style = "none";
-            return;
+            window.alert(localStorage.getItem('content'));
     }
     span.appendChild(selectedText);
     selection.insertNode(span);
@@ -73,7 +83,8 @@ function wrapSelectedText(aStyle) {
 
 /*
     ==============================================================
-                    Last Changed Function
+                    Last Changed Function. This uses the Date
+                    constructor so distinction right?
     ==============================================================
 */
 function updateDate() {
@@ -88,11 +99,85 @@ function updateDate() {
 */
 /*
     ==============================================================
+            Local Storage Functions
+    ==============================================================
+*/
+function saveContent() {
+    let titlePlaceholder = document.getElementById("fileName");
+    let textArea = document.getElementById("textArea");
+    if (titlePlaceholder.placeholder != "New Document") {
+        titlePlaceholder.value = titlePlaceholder.placeholder;
+    }
+    let titleText = (titlePlaceholder.value != "") ? titlePlaceholder.value : "New Document";
+    localStorage.setItem('title', titleText);
+    localStorage.setItem('content', textArea.innerHTML);
+}
 
+function loadContent() {
+    const savedTitle = localStorage.getItem('title');
+    const savedContent = localStorage.getItem('content');
+    const fileName = document.getElementById("fileName");
+    const textArea = document.getElementById("textArea")
+    if (savedTitle != null) {
+        fileName.placeholder = savedTitle;
+    }
+    if (savedContent != null) {
+        textArea.innerHTML = savedContent;
+    }
+    countWords(document.getElementById("textArea").innerText);
+}
+
+function resetContent() {
+    document.getElementById("fileName").placeholder = "New Document";
+    document.getElementById("textArea").innerHTML = "";
+    localStorage.setItem('title', 'New Document');
+    localStorage.setItem('content', '');
+    document.getElementById("timeChanged").innerHTML = '';
+    document.getElementById("wordCount").innerHTML = "Word Count: ";
+}
+/*
+    ==============================================================
+            End of Local storage functions
+    ==============================================================
+*/
+
+/*
+    ==============================================================
+                And finally the word count
+    ==============================================================
+*/
+function countWords(s) {
+    //window.alert(s); <- debug
+    /* s = s.replace(/(^\s*)|(\s*$)/gi, "");
+    s = s.replace(/[ ]{2,}/gi, " ");
+    s = s.replace(/\n /, "\n"); */
+    s = s.replace(/<.*?>/g, '');
+    document.getElementById("wordCount").innerHTML = "Word Count: " + s.match(/\S+/g).length;
+}
+/*
+    ==============================================================
+                    End of word count
     ==============================================================
 */
 /*
     ==============================================================
+                    Here Be Clippy
+    ==============================================================
+*/
+let counter = 0
+let bg = document.getElementById("clippyDiv").style;
 
+function clippy() {
+    if (counter === 0) {
+        bg.backgroundImage = "url('img/clippy2.gif')";
+        counter = 1;
+    } else {
+        bg.backgroundImage = "url('img/clippy.gif')";
+        counter = 0;
+    }
+}
+/*
+    ==============================================================
+                    End of Clippy
     ==============================================================
 */
